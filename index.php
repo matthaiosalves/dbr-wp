febb10ffff1cf71e62585cc4243a2acada482145
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2025-07-12 01:21:22 -0300

    feat(colunas): renderiza até 8 posts do tipo 'noticia' no carrossel, dando prioridade para categorias distintas e completando com as mais recentes

--- index.php
+++ index.php
@@ -10,118 +10,191 @@ get_header(); ?>
 
 <div class="destaques">
 	<div class="row" style="margin-bottom:0;">
+		<?php
+		$max_destaques = 6;
+		$noticias = [];
+
+		// 1º passo: pega 1 post de cada categoria, do tipo noticia
+		$categorias = get_categories([
+			'type'       => 'noticia',
+			'orderby'    => 'count',
+			'order'      => 'DESC',
+			'hide_empty' => true
+		]);
+		foreach ($categorias as $cat) {
+			$q = new WP_Query([
+				'post_type'      => 'noticia',
+				'posts_per_page' => 1,
+				'cat'            => $cat->term_id,
+				'post_status'    => 'publish',
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+				'post__not_in'   => array_column($noticias, 'ID')
+			]);
+			if ($q->have_posts()) {
+				$q->the_post();
+				$noticias[] = [
+					'ID'     => get_the_ID(),
+					'link'   => get_permalink(),
+					'img'    => get_the_post_thumbnail_url(null, 'large') ?: 'https://i.imgur.com/Ko4MtlJ.png',
+					'titulo' => get_the_title(),
+					'cat'    => get_the_category(),
+					'data'   => get_the_date('d \d\e F'),
+					'autor'  => get_the_author()
+				];
+				wp_reset_postdata();
+			}
+			if (count($noticias) >= $max_destaques) break;
+		}
+
+		// 2º passo: completa até 6 posts mais recentes (sem repetir)
+		if (count($noticias) < $max_destaques) {
+			$faltam = $max_destaques - count($noticias);
+			$q = new WP_Query([
+				'post_type'      => 'noticia',
+				'posts_per_page' => $faltam,
+				'post_status'    => 'publish',
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+				'post__not_in'   => array_column($noticias, 'ID')
+			]);
+			while ($q->have_posts()) {
+				$q->the_post();
+				$noticias[] = [
+					'ID'     => get_the_ID(),
+					'link'   => get_permalink(),
+					'img'    => get_the_post_thumbnail_url(null, 'large') ?: 'https://i.imgur.com/Ko4MtlJ.png',
+					'titulo' => get_the_title(),
+					'cat'    => get_the_category(),
+					'data'   => get_the_date('d \d\e F'),
+					'autor'  => get_the_author()
+				];
+			}
+			wp_reset_postdata();
+		}
+
+		// Divide em duas colunas de até 3
+		$colA = array_slice($noticias, 0, 3);
+		$colB = array_slice($noticias, 3, 3);
+		?>
+
 		<div class="col l6 s12" style="padding:0 .15rem;">
-			<div class="noticia" style="background-image:url('https://imgur.com/Ko4MtlJ.png');">
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4959/aberta-as-inscricoes-da-espcex">
-					<div class="categoria">EsPCEx</div>
-					<p>
-						ABERTA ÀS INSCRIÇÕES DA ESPCEx!!
-						<span class="por">
-							09 de Julho - joaovitor989
-						</span>
-					</p>
-				</a>
-			</div>
-			<div class="noticia" style="background-image:url('https://i.imgur.com/LGyzr9P.png');">
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4958/veja-os-aprovados-da-epcar-20252">
-					<div class="categoria">EPCAr</div>
-					<p>
-						Veja os aprovados da EPCAr 2025.2
-						<span class="por">
-							06 de Julho - Hiccup
-						</span>
-					</p>
-				</a>
-			</div>
-			<div class="noticia" style="background-image:url('https://i.imgur.com/Eonwg8j.png');">
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4954/venha-fazer-parte-da-laranjinha">
-					<div class="categoria">Aeronáutica no Ar</div>
-					<p>
-						Venha fazer parte da laranjinha!
-						<span class="por">
-							05 de Julho - mique_004
-						</span>
-					</p>
-				</a>
-			</div>
+			<?php foreach ($colA as $not) : ?>
+				<div class="noticia" style="background-image:url('<?php echo esc_url($not['img']); ?>');">
+					<a href="<?php echo esc_url($not['link']); ?>">
+						<div class="categoria">
+							<?php echo !empty($not['cat'][0]->name) ? esc_html($not['cat'][0]->name) : ''; ?>
+						</div>
+						<p>
+							<?php echo esc_html($not['titulo']); ?>
+							<span class="por">
+								<?php echo esc_html($not['data']); ?> - <?php echo esc_html($not['autor']); ?>
+							</span>
+						</p>
+					</a>
+				</div>
+			<?php endforeach; ?>
 		</div>
 		<div class="col l6 s12" style="padding:0 .15rem;">
-			<div class="noticia" style="background-image:url('https://i.imgur.com/LGyzr9P.png');">
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4953/se-inicia-a-epcar-20252">
-					<div class="categoria">EPCAr</div>
-					<p>
-						Se inicia a EPCAr 2025.2
-						<span class="por">
-							05 de Julho - Hiccup
-						</span>
-					</p>
-				</a>
-			</div>
-			<div class="noticia" style="background-image:url('https://i.imgur.com/Kx65qEi.png');">
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4952/inscricoes-abertas-copa-br">
-					<div class="categoria">ArenaBR</div>
-					<p>
-						INSCRIÇÕES ABERTAS - COPA BR
-						<span class="por">
-							18 de Junho - Luba012
-						</span>
-					</p>
-				</a>
-			</div>
-			<div class="noticia" style="background-image:url('https://i.imgur.com/l6qgkll_d.webp?maxwidth=760&fidelity=grand');">
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4951/feliz-aniversario-jhoao">
-					<div class="categoria">Aniversário</div>
-					<p>
-						FELIZ ANIVERSÁRIO JHOAO.:!
-						<span class="por">
-							17 de Junho - Cr.Edu16
-						</span>
-					</p>
-				</a>
-			</div>
+			<?php foreach ($colB as $not) : ?>
+				<div class="noticia" style="background-image:url('<?php echo esc_url($not['img']); ?>');">
+					<a href="<?php echo esc_url($not['link']); ?>">
+						<div class="categoria">
+							<?php echo !empty($not['cat'][0]->name) ? esc_html($not['cat'][0]->name) : ''; ?>
+						</div>
+						<p>
+							<?php echo esc_html($not['titulo']); ?>
+							<span class="por">
+								<?php echo esc_html($not['data']); ?> - <?php echo esc_html($not['autor']); ?>
+							</span>
+						</p>
+					</a>
+				</div>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </div>
 
 <div class="row" style="margin-bottom:0;">
 	<div class="col l8 s12" style="padding:0;">
+
 		<div class="colunas">
 			<h1>Colunas</h1>
-			<div class="gallery js-flickity" data-flickity-options='{ "freeScroll": true, "groupCells":true, "autoPlay": 2000, "cellAlign": "left", "prevNextButtons": false, "pageDots": false }' style="overflow:hidden;margin-top:10px;">
-				<!-- Colunas fixas (continue conforme seu HTML original) -->
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4958/veja-os-aprovados-da-epcar-20252" class="gallery-cell">
-					<div class="coluna">
-						<h5>EPCAr</h5>
-						<div class="noticia" style="background-image:url('https://i.imgur.com/LGyzr9P.png');background-repeat: no-repeat;background-size: cover;background-position: center center;">
-							Veja os aprovados da EPCAr 2025.2
+			<div class="gallery js-flickity"
+				data-flickity-options='{ "freeScroll": true, "groupCells": true, "autoPlay": 2000, "cellAlign": "left", "prevNextButtons": false, "pageDots": false }'
+				style="overflow:hidden;margin-top:10px;">
+				<?php
+				$max_colunas = 8;
+				$colunas = [];
+
+				// 1º passo: pega até 1 noticia de cada categoria (do tipo noticia)
+				$categorias = get_categories([
+					'type'       => 'noticia',
+					'orderby'    => 'count',
+					'order'      => 'DESC',
+					'hide_empty' => true
+				]);
+				foreach ($categorias as $cat) {
+					$q = new WP_Query([
+						'post_type'      => 'noticia',
+						'posts_per_page' => 1,
+						'cat'            => $cat->term_id,
+						'post_status'    => 'publish',
+						'orderby'        => 'date',
+						'order'          => 'DESC',
+						'post__not_in'   => array_column($colunas, 'ID')
+					]);
+					if ($q->have_posts()) {
+						$q->the_post();
+						$colunas[] = [
+							'ID'     => get_the_ID(),
+							'link'   => get_permalink(),
+							'img'    => get_the_post_thumbnail_url(null, 'large') ?: 'https://i.imgur.com/Ko4MtlJ.png',
+							'cat'    => get_the_category(),
+							'titulo' => get_the_title()
+						];
+						wp_reset_postdata();
+					}
+					if (count($colunas) >= $max_colunas) break;
+				}
+
+				// 2º passo: completa com as mais recentes caso não tenha 8 (sem repetir)
+				if (count($colunas) < $max_colunas) {
+					$faltam = $max_colunas - count($colunas);
+					$q = new WP_Query([
+						'post_type'      => 'noticia',
+						'posts_per_page' => $faltam,
+						'post_status'    => 'publish',
+						'orderby'        => 'date',
+						'order'          => 'DESC',
+						'post__not_in'   => array_column($colunas, 'ID')
+					]);
+					while ($q->have_posts()) {
+						$q->the_post();
+						$colunas[] = [
+							'ID'     => get_the_ID(),
+							'link'   => get_permalink(),
+							'img'    => get_the_post_thumbnail_url(null, 'large') ?: 'https://i.imgur.com/Ko4MtlJ.png',
+							'cat'    => get_the_category(),
+							'titulo' => get_the_title()
+						];
+					}
+					wp_reset_postdata();
+				}
+				?>
+
+				<?php foreach ($colunas as $col): ?>
+					<?php $cat_name = !empty($col['cat'][0]->name) ? esc_html($col['cat'][0]->name) : ''; ?>
+					<a href="<?php echo esc_url($col['link']); ?>" class="gallery-cell">
+						<div class="coluna">
+							<h5><?php echo $cat_name; ?></h5>
+							<div class="noticia"
+								style="background-image:url('<?php echo esc_url($col['img']); ?>');background-repeat: no-repeat;background-size: cover;background-position: center center;">
+								<?php echo esc_html($col['titulo']); ?>
+							</div>
 						</div>
-					</div>
-				</a>
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4954/venha-fazer-parte-da-laranjinha" class="gallery-cell">
-					<div class="coluna">
-						<h5>Aeronáutica no Ar</h5>
-						<div class="noticia" style="background-image:url('https://i.imgur.com/Eonwg8j.png');background-repeat: no-repeat;background-size: cover;background-position: center center;">
-							Venha fazer parte da laranjinha!
-						</div>
-					</div>
-				</a>
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4953/se-inicia-a-epcar-20252" class="gallery-cell">
-					<div class="coluna">
-						<h5>EPCAr</h5>
-						<div class="noticia" style="background-image:url('https://i.imgur.com/LGyzr9P.png');background-repeat: no-repeat;background-size: cover;background-position: center center;">
-							Se inicia a EPCAr 2025.2
-						</div>
-					</div>
-				</a>
-				<a href="https://diario.forcasarmadasbrhb.net/noticia/4952/inscricoes-abertas-copa-br" class="gallery-cell">
-					<div class="coluna">
-						<h5>ArenaBR</h5>
-						<div class="noticia" style="background-image:url('https://i.imgur.com/Kx65qEi.png');background-repeat: no-repeat;background-size: cover;background-position: center center;">
-							INSCRIÇÕES ABERTAS - COPA BR
-						</div>
-					</div>
-				</a>
-				<!-- ...continue todas as colunas do seu HTML igual estava... -->
+					</a>
+				<?php endforeach; ?>
 			</div>
 		</div>
 
